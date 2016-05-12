ec3d5629aa2094b98d3dd42e82a47a0373facaf1
Apollo Zhu <public-apollonian@outlook.com>
2016-05-12 15:40:16 -0400

    Basic Structure
    Use bootstrap, and add global footer using `main.js`

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function setGlobalContents(){
+	$("#header").html("")
+	$("#footer").html("<div class=\"container\"><div class=\"row\"><div class=\"text-center col-md-6 col-md-offset-3\"><h4>About</h4><p>Copyright &copy; 2016 WWITDC &middot; All Rights Reserved.</p></div></div><hr></div>");
+}
