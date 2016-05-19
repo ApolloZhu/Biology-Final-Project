0ab3b0c78250ce7c67d937db7f4ba2c3a9b18c4f
Apollo Zhu <public-apollonian@outlook.com>
2016-05-19 01:34:56 -0400

    Start Using Raphael
    Also reformat

---
+++ js/wolf.js
@@ -0,0 +1,9 @@
+function setupWolf() {
+    var canvas = Raphael($("#wolfDiagram"));
+
+    var path1 = canvas.path("M0,0L100,200T200,100L0,0").attr({
+        "fill": "#66ccff",
+        "stroke": "000000",
+        "stroke-width": 2
+    })
+}
