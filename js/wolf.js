f530cd19829802334a08ccc9b70f7f28269513cd
Apollo Zhu <public-apollonian@outlook.com>
2016-05-19 10:26:46 -0400

    Solve position error
    Now wolfDiagram is in proper place, solve #3

--- js/wolf.js
+++ js/wolf.js
@@ -1,5 +1,5 @@
 function setupWolf() {
-    var canvas = Raphael($("#wolfDiagram"));
+    var canvas = new Raphael(document.getElementById("wolfDiagram"));
 
     var path1 = canvas.path("M0,0L100,200T200,100L0,0").attr({
         "fill": "#66ccff",
