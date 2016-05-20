a436512940e91acbceaf4d2e59304334566de487
Apollo Zhu <public-apollonian@outlook.com>
2016-05-20 14:49:03 -0400

    Remove Raphael, use SVG
    Use SVG directly in html

--- js/wolf.js
+++
@@ -1,9 +0,0 @@
-function setupWolf() {
-    var canvas = new Raphael(document.getElementById("wolfDiagram"));
-
-    var path1 = canvas.path("M0,0L100,200T200,100L0,0").attr({
-        "fill": "#66ccff",
-        "stroke": "000000",
-        "stroke-width": 2
-    })
-}
